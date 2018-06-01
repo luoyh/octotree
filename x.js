9971857d3aae3a9aa50983d54478b324ed2a1f70
luoyh <luoyonghong@farpower.com>
2018-06-01 18:20:05 +0800

    +1

--- x.js
+++ x.js
@@ -3,8 +3,47 @@
 (function() {
 
 
-    console.log(location);
-    $.get('http://192.168.11.236:8080/api/v3/projects', function(r) {
-        console.log(r);
-    });
+    let url = window.location.pathname;
+    let match = url.match(/\/(\w+)\/(\w+)\/tree\/(\w+)/);
+    if (!match) {
+        return;
+    }
+    let group = match[1], project = match[2], branch = match[3];
+
+    let projects;
+
+    chrome.storage.local.get('projects', function(_prj) { projects = _prj;});
+    //debugger
+    if (!projects) {
+        $.ajax({
+            type : 'get',  
+            url : 'http://192.168.11.236:8080/api/v3/projects',
+            async : false,  
+            success : function(r){  
+                let prj = [];
+                $.each(r, function(i, e) {
+                    prj.push({
+                        id: e.id,
+                        path: e.path_with_namespace
+                    })
+                });
+                chrome.storage.local.set({
+                    'projects': prj
+                });
+                projects = {
+                    'projects': prj
+                };
+            }  
+        });
+    }
+
+    console.log(projects);
+
+    let menu = $('#ext_menu');
+    if (menu.size() == 0) {
+        $('body').append('<div id="ext_menu" style="width:100px;height:100px;background:#abcdef;"></div>');    
+    }
+
+
+    
 })();
